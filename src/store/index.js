306e91b9e1ef31c5c42f7b7e1a4ff81a02fc90bc
honeylemonade <159852.yy@163.com>
2023-04-09 15:37:49 +0800

    1

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,11 @@ export const useConfigStore = defineStore('config', {
         return {
             config: {
                 common: {
-                    backend_endpoint: "10.229.181.166:8600",
-                    db_endpoint: "10.229.181.166:8306",
-                    db_user: "root",
-                    db_password: "bce_cloud#",
-                    db_database: "objectmanager"
+                    backend_endpoint: "",
+                    db_endpoint: "",
+                    db_user: "",
+                    db_password: "",
+                    db_database: ""
                 },
                 modelConfs: []
             }
